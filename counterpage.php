39c745c9bb026098a40089cc93d681088a97f7ef
Jack Lubin <jlubin@uci.edu>
2021-05-05 13:06:48 -0700

    counter take 3

---
+++ counterpage.php
@@ -0,0 +1,20 @@
+
+
+Attempting to make a counter
+
+<br>
+
+<?php
+include "counter.php";
+$access_number = $counterVal;
+?>
+
+<footer>
+  <p>
+<?php
+    echo "You are the", $access_number, " visitor on this site!";
+    ?>
+</p>
+</footer>
+
+[home](./)
